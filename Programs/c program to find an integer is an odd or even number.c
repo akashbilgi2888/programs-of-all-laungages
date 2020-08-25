06b8c5560bc9f50c2d429a82e2f92171f627e0a7
akashbilgi2888 <57837515+akashbilgi2888@users.noreply.github.com>
2020-08-25 20:41:41 +0530

    Create c program to find an integer is an odd or even number.c

---
+++ Programs/c program to find an integer is an odd or even number.c
@@ -0,0 +1,15 @@
+
+
+/*c program to find an integer is an odd or even number */
+#include<stdio.h>
+int main()
+{
+    int n;
+    printf("enter an integer number");
+    scanf("%d",&n);
+    if(n%2==0)
+      printf("the number is an even ");
+      else
+      printf("the number is an odd");
+    return 0;
+}
